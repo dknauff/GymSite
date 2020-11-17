81842e0e8b5a9aab417db43ae49173bfe2ae1c0f
dknauff <dknauffsd@gmail.com>
2020-11-17 10:40:09 -0500

    Add project files.

---
+++ GymSite.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GymSite.WebMVC.MvcApplication" Language="C#" %>
---
+++ GymSite.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymSite.WebMVC.Startup))]
+namespace GymSite.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
